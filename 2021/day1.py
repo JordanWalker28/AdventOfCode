05496e1deca739684f3b421e4bf83ca6f0e9ab1f
Jordan Walker <jordanswalker28@gmail.com>
2021-12-10 00:06:06 +0000

    added day1 part 2 and refactored

--- 2021/day1.py
+++ 2021/day1.py
@@ -1,11 +1,13 @@
 
 from functions import *
-
+ 
 numbers = readFile('input/day1-1.txt')
 
-increases = 0
-for x in range (1 , len(numbers)):
-    if numbers[x] > numbers[x - 1]:
-        increases += 1
+slidingImage = slidingWindow(numbers, 3)
+increases = countIncreases(numbers)
+newIncreases = countIncreases(slidingImage)
         
 print(increases)
+print(newIncreases)
+
+
--- 2021/functions.py
+++ 2021/functions.py
@@ -4,3 +4,18 @@ def readFile(x):
     input = [int(entry.strip()) for entry in lines]
     return input
     
+def countIncreases(list):
+    newIncreases = 0
+    for x in range (1 , len(list)):
+        if list[x] > list[x - 1]:
+            newIncreases += 1
+    return newIncreases
+    
+def slidingWindow(list, windowSize):
+    slidingImage = []
+    currentItem = 0
+    for i in range(len(list) - windowSize + 1):
+        currentItem = sum(list[i: i + windowSize])
+        slidingImage.append(currentItem)
+    return slidingImage
+    
