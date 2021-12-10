b4036ecbd715108240da16351b3c404dcb484626
Jordan Walker <jordanswalker28@gmail.com>
2021-12-10 01:34:59 +0000

    refactored day 2

--- 2021/functions.py
+++ 2021/functions.py
@@ -23,4 +23,37 @@ def slidingWindow(list, windowSize):
         currentItem = sum(list[i: i + windowSize])
         slidingImage.append(currentItem)
     return slidingImage
+
+def workoutPositionOne(positions):
+    horizontal = 0
+    depth = 0
+    
+    for x in positions:
+        a,b = x.split(' ', 1)
+        if(a == "forward"):
+            horizontal += int(b)
+        elif(a == "up"):
+            depth -= int(b)
+        else:
+            depth += int(b)
+    return horizontal * depth
+    
+def workoutPositionTwo(positions):
+    horizontal = 0
+    depth = 0
+    aim = 0
+
+    for x in positions:
+        a,b = x.split(' ', 1)
+        if(a == "forward"):
+            horizontal += int(b)
+            if(aim == 0):
+                depth += depth * 1
+            else:
+                depth += aim * int(b)
+        elif(a == "up"):
+            aim -= int(b)
+        else:
+            aim += int(b)
+    return horizontal * depth
     
