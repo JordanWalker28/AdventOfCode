2a448306217fb4632e9146a6ddb17700c6016e16
Jordan Walker <jordanswalker28@gmail.com>
2021-12-12 21:59:44 +0000

    refactor

--- 2021/day3.py
+++ 2021/day3.py
@@ -1,26 +1,9 @@
 from functions import *
 
 lines = readTextFile("input/day3-1.txt")
-N = len(lines[0].strip())  # Number of digits in the base-2 numbers
-data = [int(line, base=2) for line in lines]
 
-bits = [2 ** n for n in range(N)]
-gamma = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit >= len(data) / 2)
-epsilon = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit <= len(data) / 2)
-print(epsilon * gamma)
+bits = createBits(lines)
+data = createDataShape(lines)
 
-
-# Part 2
-def filter_data_bitwise(data, filter_by_most_common=True):
-    filtered = [x for x in data]
-    for bit in reversed(bits):
-        ratio = sum(1 for num in filtered if num & bit) / len(filtered)
-        wanted_bit_value = bit * int((ratio >= 0.5) == filter_by_most_common)
-        filtered = [x for x in filtered if x & bit == wanted_bit_value]
-        if len(filtered) == 1:
-            break
-    return filtered
-
-filtered_by_most_common = filter_data_bitwise(data)
-filtered_by_least_common = filter_data_bitwise(data, filter_by_most_common=False)
-print(filtered_by_most_common[0] * filtered_by_least_common[0])
+print(calculateEpsilonTimesGamma(data, bits))
+print(calculateLifeSupport(data, bits))
--- 2021/functions.py
+++ 2021/functions.py
@@ -33,6 +33,34 @@ def slidingWindow(list, windowSize):
         currentItem = sum(list[i: i + windowSize])
         slidingImage.append(currentItem)
     return slidingImage
+    
+def createDataShape(lines):
+    data = [int(line, base=2) for line in lines]
+    return data
+
+def createBits(lines):
+    bits = [2 ** n for n in range(len(lines[0].strip()))]
+    return bits
+
+def calculateEpsilonTimesGamma(data, bits):
+    gamma = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit >= len(data) / 2)
+    epsilon = sum(bit for bit in bits if sum(datum & bit for datum in data) // bit <= len(data) / 2)
+    return epsilon * gamma
+
+def calculateLifeSupport(data, bits):
+    filtered_by_most_common = filter_data_bitwise(data, bits)
+    filtered_by_least_common = filter_data_bitwise(data, bits, filter_by_most_common=False)
+    return filtered_by_most_common[0] * filtered_by_least_common[0]
+
+def filter_data_bitwise(data, bits, filter_by_most_common=True):
+    filtered = [x for x in data]
+    for bit in reversed(bits):
+        ratio = sum(1 for num in filtered if num & bit) / len(filtered)
+        wanted_bit_value = bit * int((ratio >= 0.5) == filter_by_most_common)
+        filtered = [x for x in filtered if x & bit == wanted_bit_value]
+        if len(filtered) == 1:
+            break
+    return filtered
 
 def workoutPositionOne(positions):
     horizontal = 0
