a4832eb76f043877c7aab62e9f2334795aa3b561
Jordan Walker <jordanswalker28@gmail.com>
2021-12-16 13:17:39 +0000

    added file generation

--- createYearProject.py
+++ createYearProject.py
@@ -1,12 +1,25 @@
 import os
 
+def createFolders(newpath):
+    if not os.path.exists(newpath):
+        os.makedirs(newpath)
+    else:
+        print("year already exists")
+
 path = os.getcwd()
 print(path)
 year = input("Create a folder for year: ")
 print(year)
 
 newpath = path + '/' + year
-if not os.path.exists(newpath):
-    os.makedirs(newpath)
-else:
-    print("year already exists")
+createFolders(newpath)
+
+for x in range(1,26):
+    fileName = newpath + '/' + 'day' + str(x) + '.py'
+    FILE = open(fileName,"w").close()
+
+newpath = newpath + '/input'
+createFolders(newpath)
+
+
+
