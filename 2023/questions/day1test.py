bc4d5956bf9adb52b28b804133a033a2690cc3fd
Jordan Walker <jordanswalker28@gmail.com>
2023-12-02 00:23:28 +0000

    tidy up test file

--- 2023/questions/day1test.py
+++ 2023/questions/day1test.py
@@ -4,38 +4,34 @@ import os
 from day1 import *
 
 class TestCalculateTotalSum(unittest.TestCase):
-    def test_calculate_total_sum1(self):
+    def setup_file_path(self, file):
         script_dir = os.path.dirname(os.path.realpath(__file__))
         parent_dir = os.path.dirname(script_dir)
-        relative_path = os.path.join('testInput', 'day1_1.txt')
+        relative_path = os.path.join('testInput', file)
         test_file_path = os.path.join(parent_dir, relative_path)
-        expected_result = 142  
-        result = calculate_total_sum1(test_file_path)
-        self.assertEqual(result, expected_result)
+        return test_file_path
     
+    def test_calculate_total_sum1(self):
+        test_file_path = self.setup_file_path('day1_1.txt')
+        result = calculate_total_sum1(test_file_path)
+        self.assertEqual(result, 142)
+
     def test_calculate_total_sum2(self):
-        script_dir = os.path.dirname(os.path.realpath(__file__))
-        parent_dir = os.path.dirname(script_dir)
-        relative_path = os.path.join('testInput', 'day1_2.txt')
-        test_file_path = os.path.join(parent_dir, relative_path)
-        expected_result = 281  
+        test_file_path = self.setup_file_path('day1_2.txt')
         result = calculate_total_sum2(test_file_path)
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, 281)
         
     def test_get_first_and_last_digits_case1(self):
-        expected_result = 76
         result = get_first_and_last_digits("7pqrstsixteen")
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, 76)
 
     def test_get_first_and_last_digits_case2(self):
-        expected_result = 15
         result = get_first_and_last_digits("13abc45")
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, 15)
 
     def test_get_first_and_last_digits_case3(self):
-        expected_result = 42
         result = get_first_and_last_digits("42xyz")
-        self.assertEqual(result, expected_result)
+        self.assertEqual(result, 42)
 
 if __name__ == '__main__':
     unittest.main()
